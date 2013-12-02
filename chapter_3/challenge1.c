cc9a5c832df057e5f6439c75696ca62b239714be
Jeffrey <jeffreydiaz27@gmail.com>
2013-12-02 13:18:50 -0500

    Completed chanllenge 1 of chapter 3.

---
+++ chapter_3/challenge1.c
@@ -0,0 +1,15 @@
+// Challenge 1, Chapter 3
+// jeffreydiaz27@gmail.com
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+	float f1 = 3.14;
+	float f2 = 98.9;
+
+	double sum = f1 + f2;
+
+	printf("Sum is: %f\n", sum);
+	return 0;
+}
