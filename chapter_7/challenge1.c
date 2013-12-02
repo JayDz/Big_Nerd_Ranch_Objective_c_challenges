72aa027f30e5ea0096d98ef4c8232659073e0eed
Jeffrey <jeffreydiaz27@gmail.com>
2013-12-02 15:18:39 -0500

    Completed challenge for chapter 7.

---
+++ chapter_7/challenge1.c
@@ -0,0 +1,11 @@
+// Challenge 1, chapter 7
+// jeffreydiaz27@gmail.com
+
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+	printf("The sine of 1 radian is: %.3f\n", sin(1));
+	return 0;
+}
