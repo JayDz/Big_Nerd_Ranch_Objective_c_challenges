5b1d11f6e874e11f4c8973e187c9cfe72d488cba
Jeffrey <jeffreydiaz27@gmail.com>
2013-12-02 15:04:29 -0500

    Completed challenge of chapter 6.

---
+++ chapter_6/challenge1.c
@@ -0,0 +1,11 @@
+// Chanllenge 1, Chapter 6
+// jeffreydiaz27@gmail.com
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+	printf("\"5\" squared is \"%d\".\n", 5*5);
+
+	return 0;
+}
