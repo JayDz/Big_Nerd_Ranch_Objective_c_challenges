8fe019777cd0927b2147b5117bd9d3720a0fc49f
Jeffrey <jeffreydiaz27@gmail.com>
2013-12-02 15:51:32 -0500

    Completed first challenge of chapter 9.

---
+++ chapter_9/challenge1.c
@@ -0,0 +1,11 @@
+// Challenge 1, Chapter 9
+// jeffreydiaz27@gmail.com
+
+#include <stdio.h>
+
+int main(int argc, const char* argv[])
+{
+	printf("The size of float is: %zu\n", sizeof(float));
+
+	return 0;
+}
